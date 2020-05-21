d01a0ece4ae36ddb86a8e7662a5bc0c1a641dbb4
asael697 <asael.alonzo@gmail.com>
2020-05-22 01:36:32 +0200

    Roxygen2 package documentation

---
+++ R/fdaRNA-package.R
@@ -0,0 +1,21 @@
+#' The 'fdaRNA' package.
+#'
+#' @docType package
+#' @name fdaRNA-package
+#' @aliases fdaRNA
+#'
+#' @description
+#' This package is dedicated to two central aspects of gene expression preprocessing: 
+#' normalization and outlier detection. In this package, those methodologies are based 
+#' on statistical depth. The outlier detection methodology can be generally applied.
+#'
+#' @keywords  non-parametric, multivariate, robust
+#' 
+#' @import affy MASS mpm mvtnorm
+#' 
+#' @references
+#' Nieto-Reyes A, Cabrera J. Statistical depth based normalization and outlier detection of 
+#' gene expression data. Preprint
+#'
+#'
+NULL
